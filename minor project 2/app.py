2f4e6097cd796422e66ad2eac6f63f82f2ffaf61
Adarsh dwivedi <dwivediadarsh496@gmail.com>
2026-01-07 22:15:41 +0530

    Add files via upload

---
+++ minor project 2/app.py
@@ -0,0 +1,395 @@
+for i in range(1):          # run once
+    
+
+
+    print(" " * 27, end="")
+    print("%" *7 , end="")
+    print("#", end="")
+    print("*", end="")
+    print("%" * 2, end="")
+    print()
+    break
+
+    
+    for i in range(1):          # run once
+     print(" " * 24, end="")
+    print("@" * 4, end="")
+    print("%", end="")
+    print("#" * 3, end="")
+    print("*", end="")
+    print("+", end="")
+    print("*", end="")
+    print("+", end="")
+    print("#", end="")
+    print("%", end="")
+    print("#" * 3, end="")
+    print()
+    break
+
+    
+
+for i in range(1):          # run once
+    print(" " * 23, end="")
+    print("@" * 2, end="")
+    print("%" * 2, end="")
+    print("#" * 2, end="")
+    print("%" * 2, end="")
+    print("#", end="")
+    print("*" * 3, end="")
+    print("#" * 3, end="")
+    print("%" * 2, end="")
+    print("#" * 3, end="")
+    print()
+    break
+
+    
+
+for i in range(1):          # run once
+    print(" " * 22, end="")
+    print("@" * 4, end="")
+    print("%", end="")
+    print("#", end="")
+    print("*" * 2, end="")
+    print("+", end="")
+    print("=", end="")
+    print("-" * 4, end="")
+    print("=", end="")
+    print("+", end="")
+    print("#", end="")
+    print("%" * 2, end="")
+    print("#" * 2, end="")
+    print("%", end="")
+    print()
+    break
+
+  
+
+for i in range(1):          # run once
+    print(" " * 21, end="")
+    print("@", end="")
+    print("%" * 2, end="")
+    print("#" * 2, end="")
+    print("*", end="")
+    print("+", end="")
+    print("=" * 3, end="")
+    print("-" * 4, end="")
+    print(":" * 2, end="")
+    print("-" * 2, end="")
+    print("+", end="")
+    print("%", end="")
+    print("*", end="")
+    print("#", end="")
+    print("%", end="")
+    print()
+    break
+    
+
+for i in range(1):          # run once
+    print(" " * 20, end="")
+    print("@", end="")
+    print("%" * 2, end="")
+    print("#" * 2, end="")
+    print("*" * 2, end="")
+    print("+", end="")
+    print("=" * 3, end="")
+    print("-" * 2, end="")
+    print(":" * 3, end="")
+    print("-" * 4, end="")
+    print("*" * 2, end="")
+    print("%" * 2, end="")
+    print()
+    break
+
+
+for i in range(1):          # run once
+    print(" " * 20, end="")
+    print("@", end="")
+    print("%" * 3, end="")
+    print("#", end="")
+    print("*" * 2, end="")
+    print("+", end="")
+    print("=" * 2, end="")
+    print("-" * 3, end="")
+    print(":" * 3, end="")
+    print("-" * 4, end="")
+    print("=", end="")
+    print("*", end="")
+    print("%" * 2, end="")
+    print()
+    break
+
+    
+
+for i in range(1):
+    print(" " * 20, end="")
+    print("%" * 4, end="")
+    print("#", end="")
+    print("*", end="")
+    print("+" * 2, end="")
+    print("==", end="")
+    print("-", end="")
+    print("==", end="")
+    print("-" * 7, end="")
+    print("=", end="")
+    print("*", end="")
+    print("#", end="")
+    print("%", end="")
+    print(" " * 19, end="")
+    print()
+    break
+
+    
+
+for i in range(1):
+    print(" " * 20, end="")
+    print("%" * 4, end="")
+    print("#", end="")
+    print("*", end="")
+    print("#" * 3, end="")
+    print("*", end="")
+    print("+=", end="")
+    print("+", end="")
+    print("*", end="")
+    print("#" * 2, end="")
+    print("+=", end="")
+    print("-" * 3, end="")
+    print("*", end="")
+    print("#", end="")
+    print(" " * 20, end="")
+    print()
+    break
+
+    
+
+for i in range(1):
+    print(" " * 20, end="")
+    print("@", end="")
+    print("%" * 4, end="")
+    print("#", end="")
+    print("%", end="")
+    print("*" * 2, end="")
+    print("#", end="")
+    print("*", end="")
+    print("-=", end="")
+    print("+", end="")
+    print("*", end="")
+    print("%" * 2, end="")
+    print("+=", end="")
+    print("-" * 2, end="")
+    print("+=", end="")
+    print("*", end="")
+    print(" " * 19, end="")
+    print()
+    break
+
+    
+
+for i in range(1):
+    print(" " * 20, end="")
+    print("%" * 3, end="")
+    print("#" * 2, end="")
+    print("*", end="")
+    print("+" * 3, end="")
+    print("#" * 2, end="")
+    print("-" * 3, end="")
+    print("=" * 2, end="")
+    print("-" * 4, end="")
+    print("=" * 2, end="")
+    print("+", end="")
+    print(" " * 20, end="")
+    print()
+    break
+
+for i in range(1):
+    print(" " * 20, end="")
+    print("@", end="")
+    print("%" * 2, end="")
+    print("#", end="")
+    print("*", end="")
+    print("+" * 3, end="")
+    print("*", end="")
+    print("%", end="")
+    print("*", end="")
+    print("-" * 3, end="")
+    print(":" * 4, end="")
+    print("-" * 2, end="")
+    print("=+", end="")
+    print("=", end="")
+    print(" " * 20, end="")
+    print()
+    break
+
+    
+
+for i in range(1):
+    print(" " * 21, end="")
+    print("@", end="")
+    print("%" * 2, end="")
+    print("#", end="")
+    print("*", end="")
+    print("+", end="")
+    print("*", end="")
+    print("%" * 2, end="")
+    print("#", end="")
+    print("+", end="")
+    print("=" * 2, end="")
+    print("+", end="")
+    print("=", end="")
+    print("-" * 4, end="")
+    print("=-", end="")
+    print(" " * 21, end="")
+    print()
+    break
+
+
+
+for i in range(1):
+    print(" " * 22, end="")
+    print("@", end="")
+    print("%", end="")
+    print("#", end="")
+    print("*" * 2, end="")
+    print("#" * 3, end="")
+    print("*" * 2, end="")
+    print("=" * 3, end="")
+    print("+", end="")
+    print("=" * 4, end="")
+    print("+", end="")
+    print(" " * 22, end="")
+    print()
+    break
+
+ 
+
+for i in range(1):
+    print(" " * 22, end="")
+    print("@", end="")
+    print("%", end="")
+    print("#", end="")
+    print("+", end="")
+    print("#" * 2, end="")
+    print("%" * 2, end="")
+    print("#", end="")
+    print("*" * 3, end="")
+    print("+", end="")
+    print("=" * 5, end="")
+    print("+", end="")
+    print(" " * 22, end="")
+    print()
+    break
+
+
+for i in range(1):
+    print(" " * 23, end="")
+    print("%", end="")
+    print("#", end="")
+    print("*", end="")
+    print("+", end="")
+    print("*" * 2, end="")
+    print("+" * 2, end="")
+    print("=", end="")
+    print("-" * 3, end="")
+    print("=" * 4, end="")
+    print("+", end="")
+    print("*", end="")
+    print(" " * 22, end="")
+    print()
+    break
+
+
+for i in range(1):
+    print(" " * 22, end="")
+    print("@", end="")
+    print("%", end="")
+    print("#", end="")
+    print("+", end="")
+    print("#" * 2, end="")
+    print("%" * 2, end="")
+    print("#", end="")
+    print("*" * 3, end="")
+    print("+", end="")
+    print("=" * 3, end="")
+    print("+", end="")
+    print(" " * 22, end="")
+    print()
+    break
+
+
+for i in range(1):
+    print(" " * 23, end="")
+    print("%", end="")
+    print("#", end="")
+    print("*", end="")
+    print("+", end="")
+    print("*" * 2, end="")
+    print("+" * 1, end="")
+    print("=", end="")
+    print("-" * 2, end="")
+    print("=" * 2, end="")
+    print("+", end="")
+    print("*", end="")
+    print(" " * 22, end="")
+    print()
+    break
+
+
+
+for i in range(1):
+    print(" " * 20, end="")
+    print("%", end="")
+    print("%%", end="")
+    print("#" * 2, end="")
+    print("*", end="")
+    print("+" * 2, end="")
+    print("=", end="")
+    print("-" * 5, end="")
+    print("=" * 2, end="")
+    print("+", end="")
+    print("-", end="")
+    print("+", end="")
+    print("%" * 2, end="")
+    print(" " * 20, end="")
+    print()
+    break
+
+
+for i in range(1):
+    print(" " * 13, end="")
+    print("%" * 2, end="")
+    print("@" * 8, end="")
+    print("#", end="")
+    print("%", end="")
+    print("#", end="")
+    print("%", end="")
+    print("#" * 2, end="")
+    print("*", end="")
+    print("+" * 2, end="")
+    print("=" * 3, end="")
+    print("+" * 3, end="")
+    print("=:", end="")
+    print("-", end="")
+    print("%" * 4, end="")
+    print("@" * 3, end="")
+    print(" " * 15, end="")
+    print()
+    break
+
+
+
+for i in range(1):
+    print(" " * 4, end="")
+    print("@" * 19, end="")
+    print("#" * 2, end="")
+    print("%", end="")
+    print("#" * 7, end="")
+    print("*", end="")
+    print("+" * 3, end="")
+    print("=" * 2, end="")
+    print(":" * 2, end="")
+    print("%" * 6, end="")
+    print("@", end="")
+    print("%" * 3, end="")
+    print("@" * 5, end="")
+    print(" " * 2, end="")
+    print()
+    break
